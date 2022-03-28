1747f5b3e977c8221af81c6d1743a5cddffed44b
elfsvet <stepanmatysik@gmail.com>
2022-03-28 18:05:21 -0400

    Added link to a burron and function in function call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var buttonEl = document.querySelector("#save-task"); // targets button by id
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");//created new task item
+    listItemEl.className = "task-item";               // styled the new task item
+    listItemEl.textContent = "This is a new task.";   // Add the text
+    tasksToDoEl.appendChild(listItemEl);              // append this element to the task list
+}
+
+buttonEl.addEventListener("click", createTaskHandler); // adds listen click event to button
