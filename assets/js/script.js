c2ac34db551aa1ab3178176e299ed8143db5ee74
elfsvet <stepanmatysik@gmail.com>
2022-03-28 22:50:00 -0400

    added form tag with divs and options of tast type

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,24 @@ var createTaskHandler = function() {
 }
 
 buttonEl.addEventListener("click", createTaskHandler); // adds listen click event to button
+
+
+
+
+
+
+
+
+// Create a new feature branch. We'll create a new feature branch where we will build the form. ✅
+
+// Add a task form to HTML. We'll add an HTML form that will allow the user to enter the task name and type.
+
+// Handle form submission. We'll use JavaScript to add a task to the list when the "Add Task" button is clicked.
+
+// Capture form field values. We'll use JavaScript to capture the unique information the user enters (the task name and type).
+
+// Organize functionality. We'll refactor the code to make it more maintainable.
+
+// Address usability concerns. We'll improve the user experience by validating form input and resetting the form after the user clicks the "Add Task" button.
+
+// Save our progress with Git. We'll commit and push our changes up to GitHub
