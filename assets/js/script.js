d1d057aa38ba50b4110225a7f5150942489f52cc
elfsvet <stepanmatysik@gmail.com>
2022-03-30 13:34:29 -0400

    add the ability to delete a task. We used event listeners to allow the user to delete tasks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function (event) { // don't forget to put argument value as event
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-
     // check if input values are empty strings
     if (!taskNameInput || !taskTypeInput) {
         alert("You need to fill out the task form!");
@@ -17,7 +17,6 @@ var taskFormHandler = function (event) { // don't forget to put argument value a
         name: taskNameInput,
         type: taskTypeInput
     };
-
     //send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
     formEl.reset();
@@ -27,24 +26,18 @@ var createTaskEl = function (taskDataObj) {
     // create list item
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-
     // add task id as a custom attribute
     listItemEl.setAttribute("data-task-id", taskIdCounter);
-
     //create div to hold task info and add to list item
     var taskInfoE1 = document.createElement('div');
     taskInfoE1.className = 'task-info';     // give it a class name
     // add html content to div
     taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>"; // if we won't change here we will get a lexical scoping error
     listItemEl.appendChild(taskInfoE1);
-
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
-
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);              // append this element to the task list
-
-
     // Increase task counter for next unique id
     taskIdCounter++;
 };
@@ -57,39 +50,45 @@ var createTaskActions = function (taskId) {
     editButtonEl.textContent = "Edit";
     editButtonEl.className = "btn edit-btn";
     editButtonEl.setAttribute("data-task-id", taskId);
-
     actionContainerEl.appendChild(editButtonEl);
-
     // create delete button
     var deleteButtonEl = document.createElement("button");
     deleteButtonEl.textContent = "Delete";
     deleteButtonEl.className = "btn delete-btn";
     deleteButtonEl.setAttribute("data-task-id", taskId);
-
     actionContainerEl.appendChild(deleteButtonEl);
-
     var statusSelectEl = document.createElement("select");
     statusSelectEl.className = "select-status";
     statusSelectEl.setAttribute("name", "status-change");
     statusSelectEl.setAttribute("data-task-id", taskId);
-
     actionContainerEl.appendChild(statusSelectEl);
-
     var statusChoices = ["To Do", "In Progress", "Completed"];
     for (var i = 0; i < statusChoices.length; i++) {
         // create option element
         var statusOptionEl = document.createElement("option");
         statusOptionEl.textContent = statusChoices[i];
         statusOptionEl.setAttribute("value", statusChoices[i]);
-
         // append to select
         statusSelectEl.appendChild(statusOptionEl);
     }
-
     return actionContainerEl;
-}
+};
+
+var taskButtonHandler = function(event){
+    if (event.target.matches(".delete-btn")) {
+        // get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId){
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+        taskSelected.remove();
+};
 
 formEl.addEventListener("submit", taskFormHandler); // called onsubmit
+pageContentEl.addEventListener("click", taskButtonHandler);
 
 
 
