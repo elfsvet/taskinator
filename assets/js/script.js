57ce50fb74f6d0f54bf271fe5c7ae7e5f2d03d8b
elfsvet <stepanmatysik@gmail.com>
2022-03-30 15:55:34 -0400

    add ability to upgrade and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,31 +157,3 @@ var taskStatusChangeHandler = function (event) {
 formEl.addEventListener("submit", taskFormHandler); // called onsubmit
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
-
-
-
-
-
-
-
-// Create a new feature branch. We'll create a feature branch that corresponds to the GitHub issue we're addressing in this lesson.✅
-
-// Add two new lists in the HTML. We'll add a Tasks In Progress and a Tasks Completed list to Taskmaster.✅
-
-// Apply a unique id to each task. We'll create an id that uniquely identifies each task that's created.✅
-
-// Dynamically create task buttons for each task. Once we have an id for each task, we can start adding buttons and dropdowns that reference the id. Because a task and its id are dynamically created, these buttons and dropdowns will also be dynamic.✅
-
-// Add the ability to delete a task. We'll use event listeners to allow the user to delete tasks.   ✅
-
-// Load the task into the form to be edited. Users might want to edit existing tasks. We'll enable this by making sure the right task data loads in the editing form. ✅
-
-// * Loading the task's current information into the form. ✅
-
-// * On form submit, updating the task element's content. ✅
-
-// Save the edited task. Users will want to save their edits to a task; we'll make sure they can do that. ✅
-
-// Move the task based on status. If the user changes the status of a task, we'll make sure it is moved to the appropriate list. ✅
-
-// Save your progress with Git. You did it! It's time to merge your feature branch into the develop branch and commit your changes to GitHub. 
