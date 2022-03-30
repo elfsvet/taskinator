3faf797173fec458faa45a9fd1f52800c230d36b
elfsvet <stepanmatysik@gmail.com>
2022-03-29 21:23:06 -0400

    added task info and type variable elements to create new li with h3 and span elements inside div.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,25 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function (event) { // don't forget to put argument value as event
 
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    var listItemEl = document.createElement("li");//created new task item
-    listItemEl.className = "task-item";               // styled the new task item
-    listItemEl.textContent = "This is a new task.";   // Add the text
+    // create list item
+    var listItemEl = document.createElement('li');
+    listItemEl.className = 'task-item';
+
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement('div');
+    taskInfoE1.className = 'task-info';     // give it a class name
+
+    // add html content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoE1);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);              // append this element to the task list
 }
 
@@ -27,6 +39,12 @@ formEl.addEventListener("submit", createTaskHandler); // called onsubmit
 // Add a task form to HTML. We'll add an HTML form that will allow the user to enter the task name and type.
 
 // Handle form submission. We'll use JavaScript to add a task to the list when the "Add Task" button is clicked.
+
+// Target the HTML elements with the pertinent data.
+
+// Read and store the content that those elements hold.
+
+// Use that content to create a new task.
 
 // Capture form field values. We'll use JavaScript to capture the unique information the user enters (the task name and type).
 
